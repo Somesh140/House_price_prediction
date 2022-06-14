3e7b1892e0ae9578ec62bd2ead5e12d47703cd18
Somesh140 <somesh140@gmail.com>
2022-06-14 16:13:44 +0530

    new_update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting ml project"
+    return "starting ml project CI CD pipeline established"
 
 if __name__=="__main__":
     app.run(debug=True)
